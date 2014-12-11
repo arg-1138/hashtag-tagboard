4b2de21fe582c44d64073252b0465372f0025c1b
Alejandro <alejandro@nicemondays.com>
2014-12-11 17:06:24 +0100

    first commit

--- build/config.php
+++ build/config.php
@@ -1,32 +1,32 @@
 <?php
 date_default_timezone_set('Europe');
 
-//nicemondays twitter dev account
+//twitter account
 $twitter = array(
-    'consumer_key' =>           'ZsJ9pD22injM67NPQh2WlWv9V',
-    'consumer_secret' =>        'UJGk11fBYEi9JQMc8zbNaK8tvut2Polg3dXW1LleHZKRnq4oH1',
-    'access_token' =>           '2784216410-mNY2UTfOLOIzMEC6TRnQYoTrtSYxVRONYZBiiK0',
-    'access_token_secret' =>    'mrEC03cbAGlnN6GYOSZ0D3Ui0k8U7qNyKos1FsEp2Dpop'
+    'consumer_key' =>           '',
+    'consumer_secret' =>        '',
+    'access_token' =>           '',
+    'access_token_secret' =>    ''
 );
 
-//nicemondays instagram dev account
+//instagram account
 $instagram = array(
-    'client_id' =>       '8b46a7e7cf6d4261aea7a6e196024f3e',
-    'client_secret' =>  '048ad2ed63e647a7808c5a920e226417',
-    'website_url' =>    'http://nicemondays.com/proyectos/hashtag-pull/',
-    'redirect_uri' =>   'http://nicemondays.com/proyectos/hashtag-pull/instagram/',
-    'access_token' =>   '1581426559.8b46a7e.24428c65b49a4bd0a3d27cbe574c76be'
+    'client_id' =>      '',
+    'client_secret' =>  '',
+    'website_url' =>    '',
+    'redirect_uri' =>   '',
+    'access_token' =>   ''
 );
 
 $db = array(
     'host' =>           '127.0.0.1',
-    'user' =>           'nmdev',
-    'password' =>       'golondrina00',
-    'name' =>           'clapes',
+    'user' =>           'root',
+    'password' =>       'root',
+    'name' =>           'dbname',
     'media_table' =>    'media'
 );
 
-$hashtag = 'puigconnect';
+$hashtag = 'yourhashtag';
 
 define(HASHTAG_ALLOWED_ADMIN, TRUE);
 define(AUTO_UPDATE, TRUE);
